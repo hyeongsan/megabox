439e7089bba477775092e932a33a1998645810c1
san <sanstory12rt@naver.com>
2021-05-09 21:46:45 +0900

    banner responsive

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-container',{
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 5000,
+    },
+});
+
